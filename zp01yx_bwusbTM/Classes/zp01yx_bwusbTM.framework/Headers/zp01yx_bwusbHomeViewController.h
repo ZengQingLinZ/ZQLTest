ca357104dc00712f33d25ac16a10a5a9e9a2ef6e
zengqinglin <328318821@qq.com>
2019-01-23 14:08:29 +0800

    version_0.0.6

--- zp01yx_bwusbTM/Classes/zp01yx_bwusbTM.framework/Headers/zp01yx_bwusbHomeViewController.h
+++ zp01yx_bwusbTM/Classes/zp01yx_bwusbTM.framework/Headers/zp01yx_bwusbHomeViewController.h
@@ -5,8 +5,8 @@
 //  Created by Mac on 2018/2/2.
 //  Copyright © 2018年 Mac. All rights reserved.
 //
-#import <WMPageController.h>
+#import "zp01yx_bwusbWMPageController.h"
 
-@interface zp01yx_bwusbHomeViewController : WMPageController
+@interface zp01yx_bwusbHomeViewController : zp01yx_bwusbWMPageController
 
 @end
